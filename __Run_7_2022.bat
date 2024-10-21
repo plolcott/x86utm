eecdf30ed3c56b44878d4dc13a670dd3612c18a8
plolcott <59188130+plolcott@users.noreply.github.com>
2024-10-20 22:20:21 -0500

    Add files via upload

---
+++ __Run_7_2022.bat
@@ -0,0 +1,44 @@
+echo off 
+REM 2017
+REM SET PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\bin
+REM 2017
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.BAT" 
+
+REM 2022
+SET PATH=%PATH%;C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.36.32532\bin\Hostx86\x86
+echo on
+
+REM 2022
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.BAT" 
+
+REM cl /EHsc Parse_Function_Headers.cpp
+REM if ERRORLEVEL 1 pause
+REM Parse_Function_Headers Halt7.c > Halt7out.txt
+REM type Halt7out.txt
+REM if ERRORLEVEL 1 pause 
+REM if ERRORLEVEL 1 exit 
+
+REM cl.exe / O2 /EHsc x86utm.cpp api.c decode.c mem.c ops.c ops2.c prim_ops.c
+cl.exe /EHsc /std:c17 x86utm.cpp api.c decode.c mem.c ops.c ops2.c prim_ops.c
+
+if ERRORLEVEL 1 pause
+del *.obj 
+
+REM cl /FaHalt7.asm  /c /arch:IA32 /EHsc Halt7.c 
+REM cl /FaHalt7.asm  /c /arch:IA32 Halt7.c 
+REM cl     /GS-      /c /arch:IA32 Halt7.c 
+REM cl  /GS- /FaHalt7.asm  /c /arch:IA32 Halt7.c 
+REM cl  /GS- /FAcu /FaHalt7.lst  /c /arch:IA32 Halt7.c 
+cl  /GS- /FA /FaHalt7.asm /std:c17 /c /arch:IA32 Halt7.c 
+if ERRORLEVEL 1 pause
+
+REM ml -c Halt7.asm
+REM pause
+
+REM x86utm  --32 -l 0 -s 0xB4  Halt7a.bin > Halt7aout.txt
+REM x86utm  --32 -l 0 -s 0x6db Halt7.bin > Halt7out.txt
+REM x86utm  --32 -l 0 -s 0x6db Halt7.obj > Halt7out.txt
+
+REM x86utm  -b 0x1ef -s 0x508 -r 0x6df -m 100000 Halt7.obj > Halt7out.txt
+x86utm Halt7.obj > Halt7out.txt
+pause 
